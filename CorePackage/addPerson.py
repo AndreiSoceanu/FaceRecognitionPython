65f0bd5d9e70276fc08b27037f1b3c6425087840
masoceanu <masoceanu@gmail.com>
2020-03-02 17:01:27 +0200

    Added head pose tracking.

--- CorePackage/addPerson.py
+++ CorePackage/addPerson.py
@@ -19,6 +19,33 @@ while True:
     gray = clahe.apply(gray)
     gray = cv2.bilateralFilter(gray, d=15, sigmaColor=15, sigmaSpace=75)
 
+    # 3D model points.
+    # ALEA BUNE
+    model_points = np.array([
+        (0.0, 0.0, 0.0),  # Nose tip
+        (0.0, -330.0, -65.0),  # Chin
+        (-225.0, 170.0, -135.0),  # Left eye left corner
+        (225.0, 170.0, -135.0),  # Right eye right corne
+        (-150.0, -150.0, -125.0),  # Left Mouth corner
+        (150.0, -150.0, -125.0)  # Right mouth corner
+    ])
+
+    size = frame.shape
+    focal_length = size[1]
+    center = (int(size[1] / 2), int(size[0] / 2))
+    camera_matrix = np.array(
+        [[focal_length, 0, center[0]],
+         [0, focal_length, center[1]],
+         [0, 0, 1]], dtype="double"
+    )
+
+    dist_coeffs = np.zeros((4, 1))
+
+    P1 = (center[0] - 200, center[1] - 175)
+    P2 = (center[0] + 200, center[1] + 175)
+    # cv2.circle(frame,P1, 10, (0, 255,0 ), -1)
+    # cv2.circle(frame, P2, 10, (0, 255,0 ), -1)
+    frame = cv2.rectangle(frame, P1, P2, (0, 255, 0), 7)
     faces = detector(gray)
     for face in faces:
         x1 = face.left()
@@ -49,28 +76,7 @@ while True:
             rightMouthCorner  # Right mouth corner
         ], dtype="double")
 
-        # 3D model points.
 
-        model_points = np.array([
-            (0.0, 0.0, 0.0),  # Nose tip
-            (0.0, -330.0, -65.0),  # Chin
-            (-225.0, 170.0, -135.0),  # Left eye left corner
-            (225.0, 170.0, -135.0),  # Right eye right corne
-            (-150.0, -150.0, -125.0),  # Left Mouth corner
-            (150.0, -150.0, -125.0)  # Right mouth corner
-        ])
-
-        size = frame.shape
-        focal_length = size[1]
-        center = (size[1] / 2, size[0] / 2)
-
-        camera_matrix = np.array(
-            [[focal_length, 0, center[0]],
-             [0, focal_length, center[1]],
-             [0, 0, 1]], dtype="double"
-        )
-
-        dist_coeffs = np.zeros((4, 1))
 
         (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, camera_matrix,
                                                                       dist_coeffs, flags=cv2.SOLVEPNP_ITERATIVE)
@@ -80,7 +86,7 @@ while True:
 
         p1 = (int(image_points[0][0]), int(image_points[0][1]))
         p2 = (int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
-        cv2.line(frame, p1, p2, (255, 0, 0), 2)
+        cv2.arrowedLine(frame, p1, p2, (255, 0, 0), 2)
 
         # ---
 
